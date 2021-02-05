f230b8fe91541dabecaca59132921e9489dead4d
Rico <rasim.rico@gmail.com>
2021-02-05 21:24:57 +0100

    instanciating class Chart1 to display the chart

--- botify-challenge/src/App.js
+++ botify-challenge/src/App.js
@@ -1,5 +1,11 @@
+import React from "react";
+import Chart1 from "./Views/Chart1";
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Chart1 />
+    </div>
+  );
 }
 
 export default App;
---
+++ botify-challenge/src/Views/Chart1.jsx
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+import { GoogleCharts } from "google-charts";
+
+export default class Chart1 extends Component {
+  render() {
+    return <div></div>;
+  }
+}
